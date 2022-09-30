5c74299a7fa4723b26fcc983c28ea3700ecc51a9
Eyeless356 <95523501+Eyeless356@users.noreply.github.com>
2022-09-30 21:01:45 +0900

    package

--- src/Router.js
+++ src/Router.js
@@ -6,7 +6,7 @@ import Index from "./components/pages/insta/Index";
 import InstMain from "./components/pages/insta/Main";
 const Router = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/" element={<Index />}></Route>
         <Route path="/login" element={<InstaLogin />} />
